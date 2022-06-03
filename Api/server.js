0e6fae9c65fb4d185c8a9d42fad57f3772fe0c9e
karim <4bkarim.amaioua@gmail.com>
2022-06-03 12:10:13 +0100

    server setted

--- Api/server.js
+++ Api/server.js
@@ -1,1 +1,81 @@
 require('dotenv').config;
+
+const { PORT } = process.env;
+
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(morgan('dev'));
+
+app.use(express.json());
+
+app.use(fileUpload())
+
+/*
+*#################
+*## Middlewares ##
+*#################
+*/
+
+
+/*
+*######################
+*## Users Endpoints ###
+*######################
+*/
+
+
+/*
+*##########################
+*## Trainings Endpoints ###
+*##########################
+*/
+
+
+/*
+*######################
+*## Plans Endpoints ###
+*######################
+*/
+
+/*
+*######################
+*## Likes Endpoints ###
+*######################
+*/
+
+
+/*
+*######################
+*## Middleware Error ##
+*######################
+*/
+
+app.use((err, req, res, next) => {
+  console.error(err);
+
+  res.status(err.statusCode || 500).send({
+    status: 'error',
+    message: err.message,
+  });
+});
+
+/*
+*##########################
+*## Middleware Not Found ##
+*##########################
+*/
+
+app.use((req, res) => {
+  res.status(404).send({
+    status: 'error',
+    message: 'Not found',
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`);
+});
