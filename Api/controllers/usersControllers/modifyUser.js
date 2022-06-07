61205911670c277f1c4ba9264ac61428c3cc564d
isuapp <isaacmarti.g@gmail.com>
2022-06-07 11:44:04 +0200

    Codigo limpiado y comentarios añadidos

--- Api/controllers/usersControllers/modifyUser.js
+++ Api/controllers/usersControllers/modifyUser.js
@@ -3,18 +3,21 @@ const  updateUserQuery = require('../../db/userQueries/updateUserQuery');
 const modifyUser = async (req,res,next) =>{
 
     try{
+        // Destructuring del id recogido de los params
         const {id} = req.params;
-        console.log('ID', id)
+        
+        //Destructuring de los datos que recibimos del body
         const { name, email } = req.body;
-        console.log('NAME', name)
+        
+        // Llamamos a nuestra query con los parametros del body
         await updateUserQuery(id,name,email)
+
+        // Enviamos un mensaje de que todo ha ido bien
         res.send({
             status:'ok',
             message: `User ${name} updated succesfully`,
         })
 
-
-       
     }catch (error){
         next(error)
     }
--- Api/db/userQueries/updateUserQuery.js
+++ Api/db/userQueries/updateUserQuery.js
@@ -9,15 +9,17 @@ const selectUserQuery = async ( id, name, email )=>{
         //Nos conectamos
         connection = await getConnection();
 
-        // Seleccionamos el usuario con su nombre y rol
+        // Seleccionamos el usuario mediante id con su nombre y email
         const [users] = await connection.query(`SELECT name, email FROM users WHERE id = ?`, [id]);
 
+        // Si no hay usuario devolvemos un error
         if(users.length===0) throw generateError('No users found :(', 404);
 
+        // Actualizamos los datos.
         await connection.query(`
             UPDATE users SET 
             name = ?,
-            email = ?,
+            email = ?
             WHERE
             id=?
         `, [name, email, id])
